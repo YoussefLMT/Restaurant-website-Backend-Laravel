25ffbfe1e13302604523bd75fee076c76e9ca385
YoussefLMT <mcyoussef324@gmail.com>
2022-09-20 19:30:38 +0100

    add addMeal function

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -3,8 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Meal;
+use Illuminate\Support\Facades\Validator;
+
 
 class MealController extends Controller
 {
-    //
+    
+    public function addMeal(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required',
+            'price'=> 'required',
+            'category' => 'required',
+            'description' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg',
+        ]);
+
+        if($validator->fails()){
+
+            return response()->json([
+                'validation_err' => $validator->messages(),
+            ]);
+
+        }else{
+
+            $meal = new Meal;
+            $meal->name = $request->name;
+            $meal->price = $request->price;
+            $meal->category = $request->category;
+            $meal->description = $request->description;
+
+            if($request->hasFile('image'))
+            {
+                $image = $request->file('image');
+                $extension = $image->getClientOriginalExtension();
+                $image_name = time(). '.' .$extension;
+                $image->move('uploads/images', $image_name);
+                $meal->image = 'uploads/images/' . $image_name;
+            }
+
+            $meal->save();
+    
+            return response()->json([
+                'status' => 200,
+                'message' => "Meal added successfully",
+            ]);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MealController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use App\Http\Controllers\AuthController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+Route::post('add-meal', [MealController::class, 'addMeal']);
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
